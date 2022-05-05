6d43015bb231adb7c11f0e413dba216f6e343153
Evgeniy Fedorov <mailbox@z01d.net>
2022-05-05 11:15:18 +0300

    Tests pass.

--- tests/NameSpaceResolverTest.php
+++ tests/NameSpaceResolverTest.php
@@ -13,8 +13,8 @@ class NameSpaceResolverTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
 
         $resolver = new NameSpaceResolver();
-        $resolver->addPsr4Mapping("My\\Name\\Space", 'src/');
-        $resolver->addPsr4Mapping("Tests\\Space", 'tests/');
+        $resolver->addPsr4Mapping("My\\Name\\Space\\", 'src/');
+        $resolver->addPsr4Mapping("Tests\\Space\\", 'tests/');
         $resolver->path2Namespace('some/path');
 
     }
@@ -22,8 +22,8 @@ class NameSpaceResolverTest extends TestCase
     public function test_path2namespace()
     {
         $resolver = new NameSpaceResolver();
-        $nameSpacePrefix = "My\\Name\\Space";
+        $nameSpacePrefix = "My\\Name\\Space\\";
         $resolver->addPsr4Mapping($nameSpacePrefix, 'src/');
-        $this->assertEquals("$nameSpacePrefix\\dir1\\Dir2\\Dir3", $resolver->path2Namespace('src/dir1/Dir2/Dir3'));
+        $this->assertEquals("$nameSpacePrefix"."dir1\\Dir2\\Dir3", $resolver->path2Namespace('src/dir1/Dir2/Dir3'));
     }
 }
